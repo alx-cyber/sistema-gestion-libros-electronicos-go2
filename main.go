35c400546fc2fc2b969307ff5890502e7d4ab74b
alx-cyber <lexmontoyadiez@gmail.com>
2026-02-15 23:15:43 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,41 +1,40 @@
-
-
 package main
 
 import (
 	"fmt"
-	"sistema/models"
-	"sistema/services"
+	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/interfaces"
+	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/models"
+	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/services"
 )
 
 func main() {
-	var opcion int
-	libros := []models.Libro{}
 
-	for {
-		fmt.Println("\n--- Sistema de Gestión de Libros Electrónicos ---")
-		fmt.Println("1. Registrar libro")
-		fmt.Println("2. Listar libros")
-		fmt.Println("3. Buscar libro")
-		fmt.Println("4. Eliminar libro")
-		fmt.Println("5. Salir")
-		fmt.Print("Seleccione una opción: ")
-		fmt.Scanln(&opcion)
+	// Usamos la interfaz como tipo
+	var gestor interfaces.GestorLibros = services.NuevoSistema()
 
-		switch opcion {
-		case 1:
-			libros = services.RegistrarLibro(libros)
-		case 2:
-			services.ListarLibros(libros)
-		case 3:
-			services.BuscarLibro(libros)
-		case 4:
-			libros = services.EliminarLibro(libros)
-		case 5:
-			fmt.Println("Saliendo del sistema...")
-			return
-		default:
-			fmt.Println("Opción no válida")
-		}
+	// Crear libro usando constructor
+	libro1, err := models.NuevoLibro(1, "Programacion en Go", "Alex")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
+
+	// Agregar libro
+	err = gestor.AgregarLibro(libro1)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Buscar libro
+	libroEncontrado, err := gestor.BuscarLibro(1)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Libro encontrado:", libroEncontrado.Titulo())
 }
