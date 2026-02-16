8a9ac61d80aff2418a0dfbdec7b361663557c6fb
alx-cyber <lexmontoyadiez@gmail.com>
2026-02-15 23:10:15 -0500

    Create gestor.go

---
+++ interfaces(/gestor.go
@@ -0,0 +1,9 @@
+package interfaces
+
+import "github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/models"
+
+// GestorLibros define el comportamiento del sistema de gestión.
+type GestorLibros interface {
+	AgregarLibro(libro *models.Libro) error
+	BuscarLibro(id int) (*models.Libro, error)
+}
