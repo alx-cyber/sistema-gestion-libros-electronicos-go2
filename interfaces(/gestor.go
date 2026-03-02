9a348aafd1690ce3f8e622672addc6b5a89d11c9
alx-cyber <lexmontoyadiez@gmail.com>
2026-03-01 22:37:33 -0500

    Update gestor.go

--- interfaces(/gestor.go
+++ interfaces(/gestor.go
@@ -6,5 +6,6 @@ import "github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/models"
 type GestorLibros interface {
 	AgregarLibro(libro *models.Libro) error
 	BuscarLibro(id int) (*models.Libro, error)
+	ListarLibros() []*models.Libro
 	AgregarLibroConcurrente(libro *models.Libro, ch chan string)
 }
