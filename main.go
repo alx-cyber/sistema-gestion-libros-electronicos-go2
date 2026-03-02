73199ee2d28a374a0bd117574ae2b18c607778ed
alx-cyber <lexmontoyadiez@gmail.com>
2026-03-01 22:39:48 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/handlers"
 	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/interfaces"
-	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/models"
 	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/services"
 )
 
@@ -14,15 +13,13 @@ func main() {
 
 	var gestor interfaces.GestorLibros = services.NuevoSistema()
 
-	// Crear libro de prueba
-	libro1, _ := models.NuevoLibro(1, "Programacion en Go", "Alex")
-	gestor.AgregarLibro(libro1)
-
 	handler := &handlers.LibroHandler{
 		Gestor: gestor,
 	}
 
+	http.HandleFunc("/libros", handler.ListarLibros)
 	http.HandleFunc("/libro", handler.BuscarLibro)
+	http.HandleFunc("/crear", handler.CrearLibro)
 
 	log.Println("Servidor ejecutándose en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
