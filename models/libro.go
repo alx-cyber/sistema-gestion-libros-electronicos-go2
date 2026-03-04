2fccd9a81816f888eadbf9bd38eeaa3c8a2f1040
alx-cyber <lexmontoyadiez@gmail.com>
2026-03-03 19:34:23 -0500

    Update libro.go

--- models/libro.go
+++ models/libro.go
@@ -2,26 +2,25 @@ package models
 
 import "errors"
 
-// Libro representa un libro electrónico.
-// Usamos campos privados para encapsulación.
 type Libro struct {
-	titulo string
-	autor  string
-	anio   int
-	isbn   string
+	ID     int    `json:"id"`
+	Titulo string `json:"titulo"`
+	Autor  string `json:"autor"`
+	Anio   int    `json:"anio"`
+	ISBN   string `json:"isbn"`
 }
 
-// Constructor con validaciones.
-func NuevoLibro(titulo string, autor string, anio int, isbn string) (*Libro, error) {
+func NuevoLibro(id int, titulo string, autor string, anio int, isbn string) (*Libro, error) {
 	if titulo == "" || autor == "" {
 		return nil, errors.New("titulo y autor no pueden estar vacíos")
 	}
-	return &Libro{titulo: titulo, autor: autor, anio: anio, isbn: isbn}, nil
-}
 
-// Getters públicos:
-func (l *Libro) Titulo() string { return l.titulo }
-func (l *Libro) Autor() string  { return l.autor }
-func (l *Libro) Anio() int      { return l.anio }
-func (l *Libro) ISBN() string   { return l.isbn }
+	return &Libro{
+		ID:     id,
+		Titulo: titulo,
+		Autor:  autor,
+		Anio:   anio,
+		ISBN:   isbn,
+	}, nil
+}
 
