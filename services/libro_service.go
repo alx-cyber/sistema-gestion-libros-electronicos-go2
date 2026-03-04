2df1ae904acbd43c9a3a036be9aa7ac954dc3bd5
alx-cyber <lexmontoyadiez@gmail.com>
2026-03-03 22:24:27 -0500

    Update libro_service.go

--- services/libro_service.go
+++ services/libro_service.go
@@ -3,86 +3,78 @@ package services
 import (
 	"errors"
 	"sync"
+
 	"github.com/alx-cyber/sistema-gestion-libros-electronicos-go2/models"
 )
-// SistemaGestion administra los libros usando un map.
+
 type SistemaGestion struct {
 	libros map[int]*models.Libro
 	mu     sync.Mutex
 }
 
-// NuevoSistema crea una nueva instancia del sistema.
 func NuevoSistema() *SistemaGestion {
 	return &SistemaGestion{
 		libros: make(map[int]*models.Libro),
 	}
 }
 
-// AgregarLibro agrega un libro al sistema.
 func (s *SistemaGestion) AgregarLibro(libro *models.Libro) error {
-
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if _, existe := s.libros[libro.ID()]; existe {
+	if _, existe := s.libros[libro.ID]; existe {
 		return errors.New("el libro ya existe")
 	}
 
-	s.libros[libro.ID()] = libro
+	s.libros[libro.ID] = libro
 	return nil
 }
 
-// BuscarLibro busca un libro por su ID.
 func (s *SistemaGestion) BuscarLibro(id int) (*models.Libro, error) {
-
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	libro, existe := s.libros[id]
-
 	if !existe {
 		return nil, errors.New("libro no encontrado")
 	}
 
 	return libro, nil
 }
+
 func (s *SistemaGestion) AgregarLibroConcurrente(libro *models.Libro, ch chan string) {
 	err := s.AgregarLibro(libro)
-
 	if err != nil {
 		ch <- "Error: " + err.Error()
 		return
 	}
-
 	ch <- "Libro agregado correctamente"
 }
-func (s *SistemaGestion) ListarLibros() []*models.Libro {
 
+func (s *SistemaGestion) ListarLibros() []*models.Libro {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	var lista []*models.Libro
-
 	for _, libro := range s.libros {
 		lista = append(lista, libro)
 	}
-
 	return lista
 }
-func (s *SistemaGestion) ActualizarLibro(libro *models.Libro) error {
 
+func (s *SistemaGestion) ActualizarLibro(libro *models.Libro) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if _, existe := s.libros[libro.ID()]; !existe {
+	if _, existe := s.libros[libro.ID]; !existe {
 		return errors.New("libro no existe")
 	}
 
-	s.libros[libro.ID()] = libro
+	s.libros[libro.ID] = libro
 	return nil
 }
-func (s *SistemaGestion) EliminarLibro(id int) error {
 
+func (s *SistemaGestion) EliminarLibro(id int) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -93,8 +85,8 @@ func (s *SistemaGestion) EliminarLibro(id int) error {
 	delete(s.libros, id)
 	return nil
 }
-func (s *SistemaGestion) TotalLibros() int {
 
+func (s *SistemaGestion) TotalLibros() int {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
